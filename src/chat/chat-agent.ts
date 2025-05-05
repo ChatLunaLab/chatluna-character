836935a2f87b3a2d2dd6eff99a52413cb7990f0a
dingyi <dingyi222666@foxmail.com>
2025-05-05 08:37:01 +0800

    feat(chat-agent): implement ChatAgent class for enhanced chat interactions
    - Introduced the ChatAgent class to manage chat interactions with a character prompt.
    - Added logic for executing chat actions and handling outputs in a structured manner.
    - Implemented a prompt getter to initialize and manage character prompts dynamically.
    - Enhanced chain value processing to include date and time information for contextual responses.

---
+++ src/chat/chat-agent.ts
@@ -0,0 +1,61 @@
+import { BaseAgent, BaseAgentInput } from '../agent/base'
+import { CharacterPrompt } from '../agent/prompt'
+import { AgentAction } from '../agent/type'
+import { AgentFinish } from '@langchain/core/agents'
+import { PresetTemplate } from '../types'
+
+export interface ChatAgentInput extends BaseAgentInput {
+    characterPrompt: PresetTemplate
+}
+
+export class ChatAgent extends BaseAgent {
+    characterPrompt: PresetTemplate
+
+    constructor(input: ChatAgentInput) {
+        super(input)
+        this.characterPrompt = input.characterPrompt
+    }
+
+    private _prompt: CharacterPrompt
+
+    async *_execute(
+        chainValues: Record<string, unknown>
+    ): AsyncGenerator<AgentAction<'plan' | 'action' | 'finish'>> {
+        const date = new Date()
+        chainValues['weekday'] = `星期${date.getDay()}`
+        chainValues['time'] = date.toLocaleString()
+
+        chainValues['variables'] = chainValues
+
+        for await (const step of this.executor._streamIterator(chainValues)) {
+            if (step.output) {
+                const output = (step as AgentFinish)['output']
+
+                yield {
+                    type: 'finish',
+                    action: {
+                        ...(step as AgentFinish),
+                        returnValues: step,
+                        value: output
+                    }
+                }
+            } else {
+                yield {
+                    type: 'action',
+                    action: step.intermediateSteps
+                }
+            }
+        }
+    }
+
+    get prompt(): CharacterPrompt {
+        if (!this._prompt) {
+            this._prompt = new CharacterPrompt({
+                tokenCounter: (text) => this.executeModel.getNumTokens(text),
+                sendTokenLimit: 10000,
+                systemPrompt: this.characterPrompt
+            })
+        }
+        return this._prompt
+    }
+}
